9739c34d79a6f2b77179de9fe8d62b4151987ed8
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-04-18 18:55:39 +0300

    updated settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,6 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +114,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authapp.CustomUser'
