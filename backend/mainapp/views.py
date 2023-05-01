d4dc6bda59a585e2f5909c31361db08d6987a269
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-01 15:53:27 +0300

    modified view CourseListView, added CourseDetailView

--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.http import Http404
+
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, renderer_classes, action
+from rest_framework.renderers import JSONRenderer
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from authapp.models import CustomUser
@@ -6,14 +12,68 @@ from mainapp.serializers import GroupSerializer, CourseSerializer, LectureSerial
 from mainapp.models import Group, Course, Lecture, Test
 
 
-class GroupViewSet(ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+class GroupView(APIView):
+    @classmethod
+    def get_extra_actions(cls):
+        return []
+
+    def get(self, request, *args, **kwargs):
+        queryset = Group.objects.all()
+        serializer = GroupSerializer(queryset, many=True)
+
+        return Response({"group": serializer.data})
 
 
-class CourseViewSet(ModelViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+class CoursesListView(APIView):
+    @classmethod
+    def get_extra_actions(cls):
+        return []
+
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(instance=courses, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        course = request.data
+
+        serializer = CourseSerializer(data=course)
+        if serializer.is_valid(raise_exception=True):
+            course_saved = serializer.save()
+
+        return Response({"courses": "Course {} created successfully".format(course_saved.id)})
+
+    def put(self, request, pk):
+        saved_course = get_object_or_404(Course.objects.all(), pk=pk)
+        data = request.data
+        serializer = CourseSerializer(instance=saved_course, data=data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            course_saved = serializer.save()
+
+        return Response({
+            "success": "Course '{}' updated successfully".format(course_saved.id)
+        })
+
+
+class CourseDetailView(APIView):
+    @classmethod
+    def get_extra_actions(cls):
+        return []
+
+    def get(self, request, id=None):
+        if id:
+            course = Course.objects.get(id=id)
+            serializer = CourseSerializer(course)
+
+            return Response(serializer.data)
+
+        course_type = request.GET.get('course_type')
+        queryset = Course.objects.all().filter(course_type=course_type)
+        serializer = CourseSerializer(instance=queryset, many=True)
+
+        return Response(serializer.data)
 
 
 class LectureViewSet(ModelViewSet):
