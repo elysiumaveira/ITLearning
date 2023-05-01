b4e5b1e908cbd3ff90b4b06949bb8d57ebca3e1f
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-01 15:54:41 +0300

    modified CustomUserView

--- backend/authapp/views.py
+++ backend/authapp/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from authapp.serializers import CustomUserSerializer
 from authapp.models import CustomUser
 
 
-class CustomUserViewSet(ModelViewSet):
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
+class CustomUserView(APIView):
+    @classmethod
+    def get_extra_actions(cls):
+        return []
 
+    def get(self, request, *args, **kwargs):
+        queryset = CustomUser.objects.all()
+        serializer = CustomUserSerializer(queryset, many=True)
+
+        return Response({"users": serializer.data})
+
+    def post(self, request):
+        user = request.data
+
+        serializer = CustomUserSerializer(data=user)
+        if serializer.is_valid(raise_exception=True):
+            user_saved = serializer.save()
+
+        return Response({"users": "User {} created successfully".format(user_saved.id)})
+
+    def put(self, request, pk):
+        saved_user = get_object_or_404(CustomUser.objects.all(), pk=pk)
+        data = request.data
+        serializer = CustomUserSerializer(instance=saved_user, data=data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            user_saved = serializer.save()
+
+        return Response({
+            "success": "User '{}' updated successfully".format(user_saved.id)
+        })
