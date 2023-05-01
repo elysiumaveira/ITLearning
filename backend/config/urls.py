0b316b6b136fc5fd8c1dc4d6fd2b3f9d6aa93c2d
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-01 15:52:47 +0300

    modified urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from authapp.views import CustomUserViewSet
-from mainapp.views import GroupViewSet, CourseViewSet, LectureViewSet, TestViewSet
-
-
-router = DefaultRouter()
-router.register('users', CustomUserViewSet)
-router.register('groups', GroupViewSet)
-router.register('courses', CourseViewSet)
-router.register('lecture', LectureViewSet)
-router.register('tests', TestViewSet)
-
+from authapp.views import CustomUserView
+from mainapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api/', include('rest_framework.urls')),
+    # path('', include(router.urls)),
+    # path('api/', include('rest_framework.urls')),
+
+    path('users/', CustomUserView.as_view()),
+    path('users/<int:pk>/', CustomUserView.as_view(), name=""),
+
+    path('courses/', CoursesListView.as_view(), name=""),
+    path('course/', CourseDetailView.as_view(), name=""),
+    path('course/<int:id>/', CourseDetailView.as_view(), name=""),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
