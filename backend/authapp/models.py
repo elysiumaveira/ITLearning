2ba87eb57e81a53c65392e0994024463cc574695
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-15 16:23:39 +0300

    modified user model

--- backend/authapp/models.py
+++ backend/authapp/models.py
@@ -1,13 +1,49 @@
-from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
 
-class CustomUser(AbstractUser):
-    birth_date = models.DateField(blank=True, help_text=_('birth date'), verbose_name=_('Birth date'))
-    avatar = models.ImageField(upload_to='usersAvatars', null=True, blank=True, help_text=_('avatar'), verbose_name=_('Avatar'))
-    is_teacher = models.BooleanField(default=False, help_text=_('teacher status'), verbose_name=_('Teacher status'))
+class UserAccountManager(BaseUserManager):
+    def create_user(self, username, email, first_name, password=None):
+        if not username:
+            raise ValueError('Users must have an username')
+        if not email:
+            raise ValueError('Users must have an email addres')
 
-    class Meta:
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, first_name=first_name)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(max_length=150, unique=True, validators=[username_validator])
+    email = models.EmailField(max_length=255, unique=True, verbose_name=_('Email'))
+    first_name = models.CharField(max_length=255, verbose_name=_('First name'))
+    last_name = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('Last name'))
+    birth_date = models.DateField(null=True, blank=True, verbose_name=_('Birth date'))
+    avatar = models.ImageField(upload_to='usersAvatars', null=True, blank=True, verbose_name=_('Avatar'))
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name']
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
+
+    def __str__(self):
+        return self.email
