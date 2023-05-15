c1a22b8406ba5da3cf46336a2e197db90329f22d
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-15 16:24:05 +0300

    modified mainapp models

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from authapp.models import CustomUser
+from authapp.models import UserAccount
 from django.utils.translation import gettext_lazy as _
 
 
@@ -10,18 +10,21 @@ class DefaultManager(models.Manager):
 
 class Course(models.Model):
     name = models.CharField(max_length=255, verbose_name=_('Course Name'))
+    goal = models.CharField(max_length=255, verbose_name=_('Course goal'), null=True, blank=True)
     description = models.TextField(verbose_name=_('Course Description'))
-    teacher = models.ForeignKey(CustomUser, related_name='courses', on_delete=models.CASCADE, verbose_name=_('Teacher'))
+    teacher = models.ForeignKey(UserAccount, related_name='courses', on_delete=models.CASCADE, verbose_name=_('Teacher'))
     image = models.ImageField(upload_to='coursesImages', null=True, blank=True)
     course_type = models.CharField(max_length=64, verbose_name=_('Course type'), null=True, blank=True)
+    period = models.PositiveIntegerField(null=True, blank=True, verbose_name=_('Course period'))
+    price = models.PositiveIntegerField(null=True, blank=True, verbose_name=_('Course price'))
 
     objects = DefaultManager
 
 
 class Group(models.Model):
     name = models.CharField(max_length=255, verbose_name=_('Group Name'))
-    students = models.ManyToManyField(CustomUser, related_name='students', verbose_name=_('Students'))
-    teacher = models.ManyToManyField(CustomUser, related_name='approved_groups', verbose_name=_('Approved Teachers'))
+    students = models.ManyToManyField(UserAccount, related_name='students', verbose_name=_('Students'))
+    teacher = models.ManyToManyField(UserAccount, related_name='approved_teacher', verbose_name=_('Approved Teacher'))
     course = models.ForeignKey(Course, null=True, blank=True, related_name='group_course', on_delete=models.CASCADE, verbose_name=_('Course'))
 
     objects = DefaultManager
@@ -53,7 +56,7 @@ class Homework(models.Model):
 
 
 class Enrollment(models.Model):
-    student = models.ForeignKey(CustomUser, related_name='enrollments', on_delete=models.CASCADE, verbose_name=_('Student'))
+    student = models.ForeignKey(UserAccount, related_name='enrollments', on_delete=models.CASCADE, verbose_name=_('Student'))
     course = models.ForeignKey(Course, related_name='enrollments', on_delete=models.CASCADE, verbose_name=_('Course'))
     is_complete = models.BooleanField(default=False, verbose_name=_('Is Course Complete'))
 
@@ -61,7 +64,7 @@ class Enrollment(models.Model):
 
 
 class Purchase(models.Model):
-    student = models.ForeignKey(CustomUser, related_name='purchases', on_delete=models.CASCADE, verbose_name=_('Student'))
+    student = models.ForeignKey(UserAccount, related_name='purchases', on_delete=models.CASCADE, verbose_name=_('Student'))
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name=_('Course'))
 
     objects = DefaultManager
@@ -69,7 +72,7 @@ class Purchase(models.Model):
 
 class Notification(models.Model):
     text = models.TextField(verbose_name=_('Notification Text'))
-    recipients = models.ManyToManyField(CustomUser, related_name='notifications', verbose_name=_('Recipients'))
+    recipients = models.ManyToManyField(UserAccount, related_name='notifications', verbose_name=_('Recipients'))
     is_read = models.BooleanField(default=False, verbose_name=_('Is Read'))
 
     objects = DefaultManager
@@ -78,6 +81,6 @@ class Notification(models.Model):
 class Review(models.Model):
     text = models.TextField(verbose_name=_('Review Text'))
     course = models.ForeignKey(Course, related_name='reviews', on_delete=models.CASCADE, verbose_name=_('Course'))
-    author = models.ForeignKey(CustomUser, related_name='reviews', on_delete=models.CASCADE, verbose_name=_('Author'))
+    author = models.ForeignKey(UserAccount, related_name='reviews', on_delete=models.CASCADE, verbose_name=_('Author'))
 
     objects = DefaultManager
