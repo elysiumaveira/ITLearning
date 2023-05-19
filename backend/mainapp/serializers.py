ad561896bf8000c5f6654bda05b3876fe93eeac3
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-19 09:18:14 +0300

    created serializer for Lesson, Themes and Materials models

--- backend/mainapp/serializers.py
+++ backend/mainapp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from mainapp.models import Group, Course, Lecture, Test
+from mainapp.models import Group, Course, MaterialsForLesson, Themes, Lesson
 
 
 class GroupSerializer(ModelSerializer):
@@ -12,16 +12,33 @@ class GroupSerializer(ModelSerializer):
 class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = [
+            'name',
+            'goal',
+            'description',
+            'teacher',
+            'image',
+            'course_type',
+            'period',
+            'price'
+        ]
 
     def create(self, validated_data):
-        return Course.objects.create(**validated_data)
+        teachers_data = validated_data.pop('teacher')
+        course = Course.objects.create(**validated_data)
+        course.teacher.set(teachers_data)
+
+        return course
 
     def update(self, instance, validated_data):
+        teachers_data = validated_data.get('teacher', None)
+
+        if teachers_data:
+            instance.teachers.set(teachers_data)
+
         instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('description', instance.description)
         instance.image = validated_data.get('image', instance.image)
-        instance.teacher = validated_data.get('teacher', instance.teacher)
         instance.course_type = validated_data.get('course_type', instance.course_type)
         instance.period = validated_data.get('period', instance.period)
         instance.price = validated_data.get('price', instance.price)
@@ -31,19 +48,55 @@ class CourseSerializer(ModelSerializer):
         return instance
 
 
-class LectureSerializer(ModelSerializer):
+class LessonSerializer(ModelSerializer):
     class Meta:
-        model = Lecture
+        model = Lesson
         fields = '__all__'
 
     def create(self, validated_data):
-        return Lecture.objects.create(**validated_data)
+        themes_data = validated_data.pop('themes')
+        lesson = Lesson.objects.create(**validated_data)
+        lesson.themes.set(themes_data)
+
+        return lesson
+
+    def update(self, instance, validated_data):
+        themes_data = validated_data.get('themes', None)
+
+        if themes_data:
+            instance.themes.set(themes_data)
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.course = validated_data.get('course', instance.course)
+
+        instance.save()
+        return instance
 
 
-class TestSerializer(ModelSerializer):
+class ThemesSerializer(ModelSerializer):
     class Meta:
-        model = Test
+        model = Themes
         fields = '__all__'
 
     def create(self, validated_data):
-        return Test.objects.create(**validated_data)
+        materials_data = validated_data.pop('materials')
+        theme = Themes.objects.create(**validated_data)
+        theme.materials.set(materials_data)
+
+        return theme
+
+
+class MaterialsSerializer(ModelSerializer):
+    class Meta:
+        model = MaterialsForLesson
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return MaterialsForLesson.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.file = validated_data.get('file', instance.file)
+        instance.video = validated_data.get('video', instance.video)
+
+        instance.save()
+        return instance
