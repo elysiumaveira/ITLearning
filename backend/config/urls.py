5121ebe5b8e0b2e30002b9a70f0ea42dcbe849f1
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-19 09:15:15 +0300

    created new urls for materials and lessons

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
-from mainapp.views import CoursesListView, CourseDetailView, GroupView
+from mainapp.views import CoursesListView, CourseDetailView, GroupView, MaterialsView, LessonView
 
 router = DefaultRouter()
 
@@ -15,15 +15,18 @@ urlpatterns = [
 
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    re_path(r'^auth', include('djoser.urls.authtoken')),
 
-    # path('users/', CustomUserView.as_view()),
-    # path('users/<int:pk>/', CustomUserView.as_view(), name=""),
-
-    path('courses/', CoursesListView.as_view(), name=""),
-    path('course/', CourseDetailView.as_view(), name=""),
-    path('course/<int:id>/', CourseDetailView.as_view(), name=""),
+    path('courses/', CoursesListView.as_view()),
+    path('course/', CourseDetailView.as_view()),
+    path('course/<int:id>/', CourseDetailView.as_view()),
 
     path('group/', GroupView.as_view(), name=""),
+
+    path('materials/', MaterialsView.as_view()),
+    path('material/<int:id>/', MaterialsView.as_view()),
+
+    path('lessons/', LessonView.as_view())
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
