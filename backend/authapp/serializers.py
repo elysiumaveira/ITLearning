8a38652ae78f33dc0f6eb78bee53110767a77e08
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-19 09:17:30 +0300

    updated serializer

--- backend/authapp/serializers.py
+++ backend/authapp/serializers.py
@@ -1,8 +1,19 @@
+from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer
+from rest_framework.serializers import ModelSerializer
+
 from authapp.models import UserAccount
 
 
 class UserCreateSerializer(UserCreateSerializer):
+    token = serializers.CharField(max_length=255, read_only=True)
+
     class Meta(UserCreateSerializer.Meta):
         model = UserAccount
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password')
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = UserAccount
+        fields = '__all__'
