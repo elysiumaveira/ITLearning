e372630cecd53795a6b93eb97836b64cfb054625
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-25 11:12:46 +0300

    updated urls

--- backend/authapp/urls.py
+++ backend/authapp/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
 from authapp.views import UserCreateSerializer
 
 
 router = DefaultRouter()
-router.register('users', UserCreateSerializer)
+router.register('users', UserCreateSerializer, basename='user_create')
 
 urlpatterns = [
 
