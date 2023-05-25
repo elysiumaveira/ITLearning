331a9719541319cc973b95659f29c2cffc594d50
Vladislav Semenenya <vladsmnnya@gmail.com>
2023-05-25 11:12:40 +0300

    updated urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,12 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
-from mainapp.views import CoursesListView, CourseDetailView, GroupView, MaterialsView, LessonView
-
 router = DefaultRouter()
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+
     path('', include(router.urls)),
     path('api/', include('rest_framework.urls')),
 
@@ -17,16 +16,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     re_path(r'^auth', include('djoser.urls.authtoken')),
 
-    path('courses/', CoursesListView.as_view()),
-    path('course/', CourseDetailView.as_view()),
-    path('course/<int:id>/', CourseDetailView.as_view()),
-
-    path('group/', GroupView.as_view(), name=""),
-
-    path('materials/', MaterialsView.as_view()),
-    path('material/<int:id>/', MaterialsView.as_view()),
-
-    path('lessons/', LessonView.as_view())
+    path('mainapp/', include(('mainapp.urls', 'mainapp'), namespace='mainapp')),
+    path('tests/', include(('testsapp.urls', 'testsapp'), namespace='testsapp')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
